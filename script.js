de7bf8d7f49cf8a3a3317e4e605e962bf1ba684e
naum <naum43312016@gmail.com>
2020-10-22 23:08:20 +0300

    added sidebar func

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function restoreIcon(element){
     element.parentElement.getElementsByClassName("regular-icon").item(0).style.display = "inline";
     element.parentElement.getElementsByClassName("on-hover-icon").item(0).style.display = "none";
 }
+
+function toggleSidebar(behPar){
+    if(behPar==1){
+        var sidebar = document.getElementById('sidebar');
+        sidebar.style.cssText = 'display:none !important';
+        var content = document.getElementById('site-content');
+        content.className = '';
+        content.setAttribute("class", "col-12");
+        document.getElementById('menu-icon').style.display = 'inline';
+    }else{
+        var sidebar = document.getElementById('sidebar');
+        sidebar.style.cssText = 'display:block';
+        var content = document.getElementById('site-content');
+        content.className = '';
+        content.setAttribute("class", "col-lg-9 col-xl-10 col-sm-12");
+        document.getElementById('menu-icon').style.display = 'none';
+    }
+}
